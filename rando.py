35b038bc1aa1528d3ad55366ab58a02dbd876e17
Babu Baskaran <babu.b@BSB.IE>
2021-01-12 23:51:35 +0000

    started project

--- rando.py
+++ rando.py
@@ -9,14 +9,17 @@ app = fl.Flask(__name__)
 # Add root route.
 @app.route("/")
 def home():
-  return app.send_static_file('index.html')
+  #return app.send_static_file('index.html')
+  return "Hello World"
 
 # Add uniform route.
-@app.route('/api/uniform')
+#@app.route('/api/uniform')
+@app.route('/uniform')
 def uniform():
   return {"value": np.random.uniform()}
 
 # Add normal route.
-@app.route('/api/normal')
+#@app.route('/api/normal')
+@app.route('/normal')
 def normal():
   return {"value": np.random.normal()}
