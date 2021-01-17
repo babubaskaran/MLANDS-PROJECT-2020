65f8a853ca375eab9566a30d3e17642de28d557d
Babu Baskaran <babu.b@BSB.IE>
2021-01-17 00:38:08 +0000

    continue project

--- rando.py
+++ rando.py
@@ -9,7 +9,7 @@ app = fl.Flask(__name__)
 # Add root route.
 @app.route("/")
 def home():
-  return app.send_static_file('index.html')
+  return app.send_static_file('index1.html')
 
 # Add uniform route.
 @app.route('/api/uniform')
@@ -20,3 +20,12 @@ def uniform():
 @app.route('/api/normal')
 def normal():
   return {"value": np.random.normal()}
+
+
+
+@app.route('/api/model/<int:w>')
+
+def model(w):
+    # Make the prediction using our model with w, w^2, w^3.
+    p = polyreg.predict([[w, w ** 2, w ** 3]])
+    return {"value": str(p[0])} # Object must be a string.
